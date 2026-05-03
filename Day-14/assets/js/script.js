96f81e37c366ada37a58cb4cbb3c52516e67bc91
devvivekgautam-coder <dev.vivekgautam@gmail.com>
2026-05-03 22:18:14 +0530

    Completed Day-14th Project

---
+++ Day-14/assets/js/script.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById("modal");
+
+function openModal() {
+    modal.classList.add("active");
+}
+
+function closeModal() {
+    modal.classList.remove("active");
+}
+
+modal.addEventListener("click", () => {
+    closeModal();
+});
